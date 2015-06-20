97af57ae8988c55437e412392a52ab75cca65b50
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-20 18:36:50 +0530

    Added one more test for Gaussian Normalizations in case of same data

--- CodeTests/TestGaussNormalizations.cs
+++ CodeTests/TestGaussNormalizations.cs
@@ -7,6 +7,21 @@ namespace CodeTests
     [TestClass]
     public class TestGaussNormalizations
     {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "All the values entered are same, is it even a good input to predict on?")]
+        public void ExceptionThrownWhenInputDataValuesAreSame()
+        {
+            double[][] dummyData = new double[4][];
+            for (int i = 0; i < dummyData.Length; i++)
+            {
+                dummyData[i] = new double[1];
+                dummyData[i][0] = 25.00;
+            }
+
+            Normalizations.GaussNormal(dummyData, 0);
+        }
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException), "input data for Gaussian normalization is null.")]
         public void ExceptionThrownWhenInputDataIsNull()
