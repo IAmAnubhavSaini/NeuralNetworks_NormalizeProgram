04a3c77b2da4de01c6c9c63f32b01ebe546b0975
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-20 18:29:29 +0530

    Added two tests for Gaussian Normalization functionality.

---
+++ CodeTests/TestGaussNormalizations.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Normalize;
+using System;
+
+namespace CodeTests
+{
+    [TestClass]
+    public class TestGaussNormalizations
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "input data for Gaussian normalization is null.")]
+        public void ExceptionThrownWhenInputDataIsNull()
+        {
+            Normalizations.GaussNormal(null, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Input `column` cannot be less than zero.")]
+        public void ExceptionThrownWhenColumnValueIsLessThanZero()
+        {
+            double[][] dummyData = new double[4][];
+            for (int i = 0; i < dummyData.Length; i++)
+            {
+                dummyData[i] = new double[1];
+                dummyData[i][0] = 25.00;
+            }
+            Normalizations.GaussNormal(dummyData, -1);
+        }
+    }
+}
