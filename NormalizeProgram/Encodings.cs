11199b7653179cd63cd11f233fcfdecd1f77a920
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-06-20 19:56:00 +0530

    Encoding functions taken out of main program file to its own separate file. No prior tests required as the functionalities were not in use

---
+++ NormalizeProgram/Encodings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalize
+{
+    public class Encodings
+    {
+        public static string EffectsEncoding(int index, int N)
+        {
+            // If N = 3 and index = 0 -> 1,0.
+            // If N = 3 and index = 1 -> 0,1.
+            // If N = 3 and index = 2 -> -1, -1.
+            if (N == 2) // Special case.
+            {
+                if (index == 0) return "-1";
+                else if (index == 1) return "1";
+            }
+            var values = new int[N - 1];
+            if (index == N - 1) // Last item is all - 1s.
+            {
+                for (var i = 0; i < values.Length; ++i)
+                    values[i] = -1;
+            }
+            else
+            {
+                values[index] = 1; // 0 values are already there.
+            }
+            var s = values[0].ToString();
+            for (var i = 1; i < values.Length; ++i)
+                s += ", " + values[i];
+            return s;
+        }
+
+        public static string DummyEncoding(int index, int N)
+        {
+            var values = new int[N];
+            values[index] = 1;
+            var s = values[0].ToString();
+            for (var i = 1; i < values.Length; ++i)
+                s += "," + values[i];
+            return s;
+        }
+    }
+}
--- NormalizeProgram/NormalizeProgram.cs
+++ NormalizeProgram/NormalizeProgram.cs
@@ -142,40 +142,5 @@ namespace Normalize
         // sw.Close(); ofs.Close();
         // sr.Close(); ifs.Close();
         //}
-        static string EffectsEncoding(int index, int N)
-        {
-            // If N = 3 and index = 0 -> 1,0.
-            // If N = 3 and index = 1 -> 0,1.
-            // If N = 3 and index = 2 -> -1, -1.
-            if (N == 2) // Special case.
-            {
-                if (index == 0) return "-1";
-                else if (index == 1) return "1";
-            }
-            var values = new int[N - 1];
-            if (index == N - 1) // Last item is all - 1s.
-            {
-                for (var i = 0; i < values.Length; ++i)
-                    values[i] = -1;
-            }
-            else
-            {
-                values[index] = 1; // 0 values are already there.
-            }
-            var s = values[0].ToString();
-            for (var i = 1; i < values.Length; ++i)
-                s += ", " + values[i];
-            return s;
-        }
-
-        static string DummyEncoding(int index, int N)
-        {
-            var values = new int[N];
-            values[index] = 1;
-            var s = values[0].ToString();
-            for (var i = 1; i < values.Length; ++i)
-                s += "," + values[i];
-            return s;
-        }
     } // NormalizeProgram class
 } // ns
